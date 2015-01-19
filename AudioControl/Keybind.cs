f81d1d9f0e5a163f05a32bc984dacfeb047b6f86
Aaron Haun <aaron@haun.guru>
2015-01-18 21:23:35 -0500

    EOD Commit. A start on keypress handling

---
+++ AudioControl/Keybind.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioControl
+{
+    class Keybind : IDisposable
+    {
+
+        private bool registered = false;
+
+        //Parameters used for registering the hotkey
+        private int id;
+        private uint fsModifiers;
+        private uint vk;
+
+        private static delegate void KeypressHandler();
+        private KeypressHandler handler;
+
+        //This is used to register a keybind with windows
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        //This unregisters a keybind with windows.
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        public Keybind(int id, uint fsModifiers, uint vk, KeypressHandler handler)
+        {
+            this.id = id;
+            this.fsModifiers = fsModifiers;
+            this.vk = vk;
+            this.handler = handler;
+        }
+
+        public bool Register()
+        {
+            return (this.registered = RegisterHotKey((IntPtr)null, id, fsModifiers, vk));
+        }
+
+        public bool Unregister()
+        {
+            return UnregisterHotKey((IntPtr) null, id);
+        }
+
+        public void Dispose()
+        {
+            if (registered)
+                Unregister();
+        }
+    }
+}
---
+++ AudioControl/KeybindWatchThread.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace AudioControl
+{
+    class KeybindWatchThread
+    {
+
+        [DllImport("user32.dll")]
+        public static extern bool GetMessage(ref MSG message, IntPtr windowHandle, uint filterMin, uint filterMax);
+
+        private static uint WM_HOTKEY = 0x0312;
+        private static uint WM_QUIT = 0x0012;
+
+        private List<Keybind> keybinds;
+
+        public void messageLoopForever()
+        {
+            bool messageReturn;
+            MSG msg;
+            while (messageReturn = GetMessage(ref msg, (IntPtr) null, WM_HOTKEY, WM_HOTKEY))
+            {//WM_Quit can be given here, even though it was filtered out. If that happens, we should DIE!!!
+                if (msg.message == WM_QUIT)
+                {
+                    return; //We are done
+                }
+                else if (msg.message == WM_HOTKEY)
+                {
+                    //Handle the keybinds
+                }
+                else
+                {
+                    throw new InvalidOperationException(String.Format("Bad stuff happened, got a message we were not expecting {}", msg.message));
+                }
+            }
+        }
+
+        public int registerKeybind(Keybind keybind)
+        {
+            int index = keybinds.Count;
+            this.keybinds.Add(keybind);
+            if (!keybind.Register())
+            {
+                throw new InvalidOperationException("Something bad happend while registering a keybind!");
+            }
+            return index;
+        }
+
+    }
+}
