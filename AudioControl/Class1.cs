22d2c3e474f2da5a3501173edcde0d5344240695
Aaron Haun <aaron@haun.guru>
2015-01-18 20:20:39 -0500

    Initial skeleton

---
+++ AudioControl/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioControl
+{
+    class AudioControlApplication
+    {
+        int Main(int argc, int argv)
+        {
+            return 0;
+        }
+    }
+}
